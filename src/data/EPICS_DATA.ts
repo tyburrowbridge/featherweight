eab4c25e3d428c7ef2dec6706ccb1a177eb06fda
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-02-08 17:14:33 -0500

    tag pills

---
+++ src/data/EPICS_DATA.ts
@@ -0,0 +1,7 @@
+export const EPICS_DATA = [
+  {
+    label: 'Keep things crispy',
+    story:
+      'As a user I want things to be super crispy, because it gives ultimate joy to life.'
+  }
+]
---
+++ src/js/components/TagPill.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface TagPillProps {
+  label: string
+}
+
+export const TagPill: React.SFC<TagPillProps> = props => {
+  return <div className="tag-pill">{props.label}</div>
+}
+
+export default TagPill
