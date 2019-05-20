9b9a23315050f3dda04bcae4ba4a6edb09f1f3ea
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-05-20 08:20:25 -0400

    added react router to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './store/reducers/index'
-import './index.css'
-import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-const store = createStore(reducer)
+import { BrowserRouter } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { appReducer } from './store/reducers/reducer'
+
+import { App } from './App'
+
+import './index.css'
+
+const store = createStore(appReducer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister()
