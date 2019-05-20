dd5a0788a0aa24f40d0d035415b54f4ffc5ad953
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-05-20 08:20:03 -0400

    added ids to epcis mock data

--- src/data/EPICS_DATA.ts
+++ src/data/EPICS_DATA.ts
@@ -1,7 +1,24 @@
 export const EPICS_DATA = [
   {
+    id: 1,
     label: 'The featherweight kanban',
     story:
       'As a small team we want a super light way manage our task and build working product.'
+  },
+  {
+    id: 2,
+    label: 'Massive Refactor',
+    story: 'As a wannbe engineer I need to refactor in order to get better'
+  },
+  {
+    id: 3,
+    label: 'Create an Account',
+    story:
+      'As a user I would like to create and account so that I can save my data'
+  },
+  {
+    id: 4,
+    label: 'Epic List',
+    story: 'As a user I would like a top level list of all of my epics'
   }
 ]
