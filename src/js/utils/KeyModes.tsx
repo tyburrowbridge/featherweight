129fdeb2fae411ec28b7570e20da6ad72ddd6d3b
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-05-20 08:23:25 -0400

    added KeyModes and added redux

--- src/js/utils/KeyModes.tsx
+++ src/js/utils/KeyModes.tsx
@@ -1,38 +1,50 @@
+// imports
 import React from 'react'
-// import { DetectKeyPress } from './DetectKeyPress'
 import { connect } from 'react-redux'
 import { useState, useEffect } from 'react'
-import { NORMAL_MODE, VISUAL_MODE, INSERT_MODE } from '../components/Modes'
+import { NORMAL_MODE, VISUAL_MODE, INSERT_MODE } from '../../data/MODES'
+import { ModeBar } from '../components/ModeBar'
 
-interface KeyModeProps {
+// variables
+const normal = NORMAL_MODE.name
+const normalKey = NORMAL_MODE.key
+const visual = VISUAL_MODE.name
+const visualKey = VISUAL_MODE.key
+const insert = INSERT_MODE.name
+const insertKey = INSERT_MODE.key
+
+type KeyModeProps = {
   normalMode: () => {}
   visualMode: () => {}
   insertMode: () => {}
+  incrementCardSelected: () => {}
+  cardSelected: number
+  mode: string
 }
 
 const KeyModes: React.FC<KeyModeProps> = ({
   normalMode,
   visualMode,
-  insertMode
+  insertMode,
+  incrementCardSelected,
+  cardSelected,
+  mode
 }) => {
-  const normalModeKey = 78 // n //
-  const visualModeKey = 86 // v //
-  const insertModeKey = 73 // i //
-
   const [keyPressed, setKeyPressed] = useState(false)
 
   const downHandler = (key: any) => {
-    console.log(`key down        : [${key.keyCode}]`)
-    key.keyCode === normalModeKey ? normalMode() : null
-    key.keyCode === visualModeKey ? visualMode() : null
-    key.keyCode === insertModeKey ? insertMode() : null
+    key.keyCode === normalKey ? (normalMode(), setKeyPressed(true)) : null
+    key.keyCode === visualKey ? (visualMode(), setKeyPressed(true)) : null
+    key.keyCode === insertKey ? (insertMode(), setKeyPressed(true)) : null
+    incrementCardSelected()
+    console.log(`key pressed :   ${key.keyCode}`)
   }
 
   // If released key is our target key then set to false
   const upHandler = (key: any) => {
-    key.keyCode === normalModeKey ? setKeyPressed(false) : null
-    key.keyCode === visualModeKey ? setKeyPressed(false) : null
-    key.keyCode === insertModeKey ? setKeyPressed(false) : null
+    key.keyCode === normalKey ? setKeyPressed(false) : null
+    key.keyCode === visualKey ? setKeyPressed(false) : null
+    key.keyCode === insertKey ? setKeyPressed(false) : null
   }
 
   // Add event listeners
@@ -46,19 +58,20 @@ const KeyModes: React.FC<KeyModeProps> = ({
       window.removeEventListener('keyup', upHandler)
     }
   }, []) // Empty array ensures that effect is only run on mount
-
-  return <div />
+  return <ModeBar currentMode={mode} currentCard={cardSelected} />
 }
-
-const mapStateToProps = (state: any) => {
-  return { mode: state.mode }
+const mapStateToProps = (state: any, ownProps: any) => {
+  return {
+    mode: state.mode,
+    cardSelected: state.cardSelected
+  }
 }
-
 const mapDispatchToProps = (dispatch: any) => {
   return {
-    normalMode: () => dispatch({ type: NORMAL_MODE }),
-    visualMode: () => dispatch({ type: VISUAL_MODE }),
-    insertMode: () => dispatch({ type: INSERT_MODE })
+    normalMode: () => dispatch({ type: normal }),
+    visualMode: () => dispatch({ type: visual }),
+    insertMode: () => dispatch({ type: insert }),
+    incrementCardSelected: () => dispatch({ type: 'INCREMENT_CARD' })
   }
 }
 
@@ -66,8 +79,3 @@ export const SetKeyModes = connect(
   mapStateToProps,
   mapDispatchToProps
 )(KeyModes)
-
-// return <DetectKeyPress targetKey={78} setMode={setKeyModes} />
-// return <DetectKeyPress targetKey={78} mode={Modes.normal} />
-// setMode: () => dispatch({ type: NORMAL_MODE })
-// <DetectKeyPress targetKey={78} setMode={setKeyModes} mode={NORMAL_MODE} />
