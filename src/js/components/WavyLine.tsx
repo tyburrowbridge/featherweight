5148118b10c1b15c35cdf4ebbda955a6c8d7bdb0
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-05-20 08:25:54 -0400

    removing some useless logs

--- src/js/components/WavyLine.tsx
+++ src/js/components/WavyLine.tsx
@@ -5,7 +5,6 @@ interface WavyLineProps {
 }
 
 export const WavyLine: React.FC<WavyLineProps> = props => {
-  console.log(`wavy visible :   ${props.visible}`)
   return (
     <div
       className={`wavy-line wavy-line${props.visible ? '--show' : '--hide'}`}
