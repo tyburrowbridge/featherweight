36457b94d385432e206c2d46efefff6c8104454a
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-05-20 08:20:12 -0400

    refactored modes

--- src/data/MODES.ts
+++ src/data/MODES.ts
@@ -1,3 +1,17 @@
-export const NORMAL_MODE = 'NORMAL_MODE'
-export const VISUAL_MODE = 'VISUAL_MODE'
-export const INSERT_MODE = 'INSERT_MODE'
+export const NORMAL_MODE = {
+  name: 'NORMAL_MODE',
+  key: 78
+  // n //
+}
+
+export const VISUAL_MODE = {
+  name: 'VISUAL_MODE',
+  key: 86
+  // v //
+}
+
+export const INSERT_MODE = {
+  name: 'INSERT_MODE',
+  key: 73
+  // i //
+}
