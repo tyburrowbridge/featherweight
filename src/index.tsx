f563e52cda77eb6fe633e4941d8c4ea0966c5aa3
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-06-07 16:10:57 -0400

    setup appsync client HOC

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,10 @@ import * as serviceWorker from "./serviceWorker"
 
 import { BrowserRouter } from "react-router-dom"
 
-// TODO: install aws-appsync
-// import Client from 'aws-appsync'
-// import { ApolloProvider } from 'react-apollo'
-// import { Rehydrated } from 'aws-appsync-react'
-// import AppSync from './AppSync'
+import Client from "aws-appsync"
+import { ApolloProvider } from "react-apollo"
+import { Rehydrated } from "aws-appsync-react"
+import AppSync from "./AppSync"
 
 import { Provider } from "react-redux"
 import { createStore } from "redux"
@@ -19,15 +18,26 @@ import { App } from "./App"
 import "./index.css"
 
 const store = createStore(appReducer)
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-
-  document.getElementById("root")
-)
+const client = new Client({
+  url: AppSync.aws_appsync_graphqlEndpoint,
+  region: AppSync.aws_appsync_region,
+  auth: {
+    type: "API_KEY",
+    apiKey: AppSync.aws_appsync_apiKey
+  }
+})
 
 serviceWorker.unregister()
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </Rehydrated>
+  </ApolloProvider>
+)
+
+ReactDOM.render(<WithProvider />, document.getElementById("root"))
