78649ead41781b4893f22ba13f7ca2e8082c16a6
tyburrowbridge <8630084+tyburrowbridge@users.noreply.github.com>
2019-06-07 16:12:44 -0400

    started adding graphql & appsync to the app
    loads more work to do here, but this is the start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
-import * as React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import * as React from "react"
+import { Route, Switch } from "react-router-dom"
 
-import { EpicView } from './js/views/EpicView'
-import { NewCardView } from './js/views/NewCardView'
+import { EpicView } from "./js/views/EpicView"
+import { NewCardView } from "./js/views/NewCardView"
 
-import './scss/_base.scss'
+import gql from "graphql-tag"
+import { graphql, compose } from "react-apollo"
+import { graphqlMutation } from "aws-appsync-react"
+import { buildSubscription } from "aws-appsync"
+
+import "./scss/_base.scss"
+
+const SubscribeToTodos = gql`
+  subscription {
+    onCreateTodo {
+      id
+      title
+      completed
+    }
+  }
+`
+// TODO: use reference to implement cards
+// const Item = (key: any, title: any) => (
+//   <div key={key}>
+//     <p>{title}</p>
+//   </div>
+// )
+//
+// const App: React.FC<AppProps> = (props) => {
+//   useEffect(() => {
+//     props.subscribeToMore(buildSubscription(SubscribeToTodos, ListTodos), [
+//       ListTodos
+//     ])
+//   })
+//
+//   const [value, setValue] = useState("")
+//
+//   const addTodo = () => {
+//     const todo = { title: value, completed: false }
+//
+//     if (value === "") {
+//       return
+//     }
+//
+//     props.createTodo(todo)
+//     setValue("")
+//   }
+//
+//   return (
+//     <div className="App">
+//       <div>
+//         <input
+//           onChange={(e) => setValue(e.target.value)}
+//           value={value}
+//           placeholder="add a todo"
+//         />
+//         <button onClick={() => addTodo()}> Add Todo Item </button>
+//       </div>
+//       <div>
+//         {props.todos.map((item: any, i: number) => Item(i, item.title))}
+//       </div>
+//     </div>
+//   )
+// }
 
 export const App = () => {
   return (
@@ -18,3 +76,17 @@ export const App = () => {
 }
 
 export default App
+
+// TODO: replace app default export to compose the graphqlMutation jawn.
+// export default compose(
+//   graphqlMutation(CreateTodo, ListTodos, "Todo"),
+//   graphql(ListTodos, {
+//     options: {
+//       fetchPolicy: "cache-and-network"
+//     },
+//     props: (props: any) => ({
+//       subscribeToMore: props.data.subscribeToMore,
+//       todos: props.data.listTodos ? props.data.listTodos.items : []
+//     })
+//   })
+// )(App)
